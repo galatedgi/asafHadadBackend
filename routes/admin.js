0fad10209a3c2b46dd3d94f0e676c05ee7711404
galatedgi <gal.atedgi@gmail.com>
2021-05-12 20:08:17 +0300

    Gal

--- routes/admin.js
+++ routes/admin.js
@@ -91,27 +91,56 @@ router.post("/login", async (req, res, next) => {
       }
     });
 
-    router.post("/register", async (req, res, next) => {
+    // router.post("/register", async (req, res, next) => {
+    //   try {
+    //     // const users = await DButils.execQuery("SELECT username FROM dbo.users");
+    //     // if (users.find((x) => x.username === req.body.username))
+    //     //   throw { status: 400, message: "Username taken" };
+    //     // if (req.body.passwordAgain != req.body.password)
+    //     //   throw { status: 400, message: "Passwords is not identical." };
+    //     let hash_password = Bcryptjs.hashSync(req.body.password,14);
+    //     let username= req.body.username;
+    //     await DButils.execQuery(
+    //       `INSERT INTO dbo.admins (username, password) VALUES ('${req.body.username}', '${hash_password}')`
+    //     );
+    //     res.status(201).send("user created");
+    //   } catch (error) {
+    //     console.log(error);
+    //     if(error.status){
+    //     res.status(error.status).send(error.message);
+    //     }else{
+    //       res.status(500).send(error);
+    //     }
+    //   }
+    // });
+
+    router.post("/unlock", async (req, res) => {
       try {
-        // const users = await DButils.execQuery("SELECT username FROM dbo.users");
-        // if (users.find((x) => x.username === req.body.username))
-        //   throw { status: 400, message: "Username taken" };
-        // if (req.body.passwordAgain != req.body.password)
-        //   throw { status: 400, message: "Passwords is not identical." };
-        let hash_password = Bcryptjs.hashSync(req.body.password,14);
-        let username= req.body.username;
-        await DButils.execQuery(
-          `INSERT INTO dbo.admins (username, password) VALUES ('${req.body.username}', '${hash_password}')`
-        );
-        res.status(201).send("user created");
+          let date = req.body.date;
+          let time = req.body.time;
+          let status=req.body.status;
+          const turns = await DButils.execQuery(`SELECT [status],[user] FROM dbo.Queues where [time]='${time}' and [date]='${date}'`);
+          if(turns[0].user!=null){
+            res.status(406).send({ message: "The turn is allrady set to "+turns[0].user });
+          }
+          else if(turns[0].unlock=="y"){
+          await DButils.execQuery(
+            `update dbo.Queues set [unlock]='n' where [time]='${time}' and [date]='${date}'`
+          );
+          res.status(200).send({ message: "The queue was successfully canceled", success: true });
+        }else{
+          await DButils.execQuery(
+            `update dbo.Queues set [unlock]='y' where [time]='${time}' and [date]='${date}'`
+          );
+          res.status(200).send({ message: "The queue was successfully opened", success: true });
+        }
       } catch (error) {
         console.log(error);
         if(error.status){
         res.status(error.status).send(error.message);
         }else{
           res.status(500).send(error);
-        }
-      }
+        }  }
     });
 
   module.exports = router;
